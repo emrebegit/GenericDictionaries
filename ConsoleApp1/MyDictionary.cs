ec4c3e24089a804cf0647e97b7e5dd7534a21be6
aokuvel <aokuvel@gmail.com>
2021-01-23 20:23:36 +0300

    First

---
+++ ConsoleApp1/MyDictionary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MyDictionary<T>
+    {
+        Dictionary<string,List<T>> d=new Dictionary<string,List<T>>();
+        T[] items;
+        public MyDictionary()
+        {
+            items = new T[0];
+        }
+        public void Add(string key,List<T> list)
+        {
+            d.Add(key, list);
+        }
+        public void DictPrinter()
+        {
+            foreach (KeyValuePair<string, List<T>> kvp in d)
+            {
+                Console.WriteLine("Key={0}, Value={1}", kvp.Key, kvp.Value[0]);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+            List<string> list=new List<string>();
+            list.Add("05050");
+            isimler.Add("Engin",list);
+
+            List<string> liste = new List<string>();
+            Console.WriteLine(liste.Count);
+            isimler.DictPrinter();
+            Console.ReadKey();
+        }
+    }
+}
